e5bca47b1103a68c930427422814ff980bc76637
Michael <mglinares.2419@gmail.com>
2024-01-17 09:59:08 -0400

    first commit

--- lib/lambda-stepfunction-demo-stack.ts
+++ lib/lambda-stepfunction-demo-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { JsonPath } from 'aws-cdk-lib/aws-stepfunctions';
 import path = require('path');
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -65,6 +66,96 @@ export class LambdaStepfunctionDemoStack extends cdk.Stack {
     // Add a new POST route to the REST API Gateway, and link it to the createStoreItem Lambda function
     const createStoreItemResource = myFirstApi.root.addResource('create-store-item');
     createStoreItemResource.addMethod('POST', new cdk.aws_apigateway.LambdaIntegration(createStoreItem));
+
+    // Create a Map task, iterating over the items of the input
+    const isItemInStockMappedTask = new cdk.aws_stepfunctions.Map(this, 'isItemInStockMappedTask', {
+      itemsPath: '$.order',
+      resultPath: JsonPath.DISCARD,
+      parameters: {
+        'item.$': '$$.Map.Item.Value',
+      },
+    }).iterator(
+      new cdk.aws_stepfunctions_tasks.LambdaInvoke(this, 'isItemInStockTask', {
+        lambdaFunction: isItemInStock,
+      }),
+    );
+
+    // Create a Map task, iterating over the items of the input
+    const updateItemStockMappedTask = new cdk.aws_stepfunctions.Map(this, 'updateItemStockMappedTask', {
+      itemsPath: '$.order',
+      resultPath: JsonPath.DISCARD,
+      parameters: {
+        'item.$': '$$.Map.Item.Value',
+      },
+    }).iterator(
+      new cdk.aws_stepfunctions_tasks.LambdaInvoke(this, 'updateItemStockTask', {
+        lambdaFunction: updateItemStock,
+      }),
+    );
+
+    // Create simple task, calling the createOrder Lambda function
+    const createOrderTask = new cdk.aws_stepfunctions_tasks.LambdaInvoke(this, 'createOrderTask', {
+      lambdaFunction: createOrder,
+    });
+
+    const parallelState = new cdk.aws_stepfunctions.Parallel(this, 'parallelState', {});
+
+    parallelState.branch(updateItemStockMappedTask, createOrderTask);
+
+    const definition = isItemInStockMappedTask.next(parallelState);
+
+    const myFirstStateMachine = new cdk.aws_stepfunctions.StateMachine(this, 'myFirstStateMachine', {
+      definition,
+    });
+
+    const invokeStateMachineRole = new cdk.aws_iam.Role(this, 'invokeStateMachineRole', {
+      assumedBy: new cdk.aws_iam.ServicePrincipal('apigateway.amazonaws.com'),
+    });
+    
+    invokeStateMachineRole.addToPolicy(
+      new cdk.aws_iam.PolicyStatement({
+        actions: ['states:StartExecution'],
+        resources: [myFirstStateMachine.stateMachineArn],
+      }),
+    );
+
+    const createOrderResource = myFirstApi.root.addResource('create-order');
+
+createOrderResource.addMethod(
+  'POST',
+  new cdk.aws_apigateway.Integration({
+    type: cdk.aws_apigateway.IntegrationType.AWS,
+    integrationHttpMethod: 'POST',
+    uri: `arn:aws:apigateway:${cdk.Aws.REGION}:states:action/StartExecution`,
+    options: {
+      credentialsRole: invokeStateMachineRole,
+      requestTemplates: {
+        'application/json': `{
+        "input": "{\\"order\\": $util.escapeJavaScript($input.json('$'))}",
+        "stateMachineArn": "${myFirstStateMachine.stateMachineArn}"
+      }`,
+      },
+      integrationResponses: [
+        {
+          statusCode: '200',
+          responseTemplates: {
+            'application/json': `{
+            "statusCode": 200,
+            "body": { "message": "OK!" }"
+          }`,
+          },
+        },
+      ],
+    },
+  }),
+  {
+    methodResponses: [
+      {
+        statusCode: '200',
+      },
+    ],
+  },
+);
   }
 }
 
